8e31b097c96787ecd650e478a22c14e2562cff9a
jisuna314 <jisuna314@gmail.com>
2024-09-05 16:22:50 +0900

    Add : 파일 업로드, 다운로드 api 추가

--- src/main/java/com/example/multidata/security/CustomUserDetails.java
+++ src/main/java/com/example/multidata/security/CustomUserDetails.java
@@ -2,19 +2,19 @@ package com.example.multidata.security;
 
 import java.util.Collection;
 
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.multidata.entity.User;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 @Getter
 @RequiredArgsConstructor
+@Setter
 public class CustomUserDetails implements UserDetails {
 
     private final User user;
+    private String tenantId;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
